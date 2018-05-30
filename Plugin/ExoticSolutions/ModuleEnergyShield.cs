11a2c1e658ac93180a22b9c7820a5b1c39cef3b9
Darinth <cerka@marshall.edu>
2018-05-30 19:14:56 -0400

    Most recent version of ModuleEnergyShield Vessels still experiencing RUD if shield started in off state and is turned on.

--- Plugin/ExoticSolutions/ModuleEnergyShield.cs
+++ Plugin/ExoticSolutions/ModuleEnergyShield.cs
@@ -6,25 +6,93 @@ using UnityEngine;
 
 namespace ExoticSolutions
 {
-    class ModuleEnergyShield : PartModule
+    class ModuleEnergyShield : PartModule, IAirstreamShield
     {
         GameObject shieldObject;
+
+        [KSPField(isPersistant = true)]
+        bool shieldOn = false;
+
+        bool updateShield = true;
 
         public override void OnAwake()
         {
             base.OnAwake();
             shieldObject = gameObject.GetChild("SphereShield");
             KSPLog.print("shieldObject: " + shieldObject);
-            shieldObject.SetActive(false);
-            part.DragCubes.SetCubeWeight("Shield", 0);
+            //shieldObject.SetActive(false);
         }
 
-        [KSPEvent(active = true, guiActive = true, guiActiveEditor = false, guiActiveUncommand = false, guiName = "LOL Shield", name = "LOLShield", requireFullControl = true)]
+        public override void OnInitialize()
+        {
+            base.OnInitialize();
+            /*if (!shieldOn)
+            {
+                part.DragCubes.SetCubeWeight("Default", 1);
+                part.DragCubes.SetCubeWeight("Shield", 0);
+                shieldObject.SetActive(false);
+            }
+            else
+            {
+                part.DragCubes.SetCubeWeight("Default", 0);
+                part.DragCubes.SetCubeWeight("Shield", 1);
+            }*/
+        }
+
+        [KSPEvent(active = true, guiActive = true, guiActiveEditor = true, guiActiveUncommand = false, guiName = "LOL Shield", name = "LOLShield", requireFullControl = true)]
         public void LOLShield()
         {
-            Vector3 velocity = part.Rigidbody.velocity;
-            shieldObject.SetActive(true);
-            part.Rigidbody.velocity = velocity;
+            shieldOn = !shieldOn;
+            updateShield = true;
+            //shieldObject.transform.localPosition = gameObject.GetChild("ksp_s_processorSmall_fbx").transform.localPosition;
+            //shieldObject.transform.localPosition = part.boundsCentroidOffset;
+            //shieldObject.transform.localPosition = part.CenterOfDisplacement;
+            //Vector3 velocity = part.Rigidbody.velocity;
+            //shieldObject.SetActive(true);
+            //part.Rigidbody.velocity = velocity;
+            //foreach(Part p in vessel.parts)
+            //{
+            //p.AddShield(this);
+            //}
+        }
+
+        public void FixedUpdate()
+        {
+            if (updateShield)
+            {
+                if (shieldOn)
+                {
+                    updateShield = false;
+                    part.DragCubes.SetCubeWeight("Default", 0);
+                    part.DragCubes.SetCubeWeight("Shield", 1);
+                    shieldObject.SetActive(true);
+                    part.Update();
+                }
+                else
+                {
+                    updateShield = false;
+                    part.DragCubes.SetCubeWeight("Default", 1);
+                    part.DragCubes.SetCubeWeight("Shield", 0);
+                    shieldObject.SetActive(false);
+                    part.Update();
+                }
+            }
+
+        }
+
+        public bool ClosedAndLocked()
+        {
+            return true;
+        }
+
+        public Vessel GetVessel()
+        {
+            return vessel;
+        }
+
+        public Part GetPart()
+        {
+            return part;
         }
     }
 }
